9936c64e5a80b3137a76bec0509e7bb9af774520
Dom Sabatini <domsab@hotmail.com>
2024-04-30 11:45:45 +0100

    Fixes broken test fake

--- tests/Feature/KayneRestManagerTest.php
+++ tests/Feature/KayneRestManagerTest.php
@@ -5,12 +5,10 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Quotes;
-use App\QuotesManager;
 use App\Services\KanyeRestService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class KayneRestManagerTest extends TestCase
 {
@@ -48,29 +46,30 @@ class KayneRestManagerTest extends TestCase
         $response->assertStatus(401);
     }
 
-    public function test_user_can_get_quotes(): void
+    public function test_user_can_get_five_unique_quotes(): void
     {
         Http::fake([
-            KanyeRestService::KANYE_REST_ENDPOINT => Http::response([
-                'type'       => 'example',
-                'id'         => 'some id',
-                'attributes' => [
-                    'email'      => 'some email',
-                    'uuid'       => 'some uuid',
-                    'created_at' => 'some created_at',
-                    'updated_at' => 'some updated_at',
-                ],
-            ], 200),
+            KanyeRestService::KANYE_REST_ENDPOINT => Http::sequence()
+                ->push('example1', 200)
+                ->push('example2', 200)
+                ->push('example3', 200)
+                ->push('example4', 200)
+                ->push('example4', 200)
+                ->push('example5', 200)
         ]);
 
         $user = User::factory()->create();
         Auth::loginUsingId($user->id);
 
         $response = $this->withToken($user->api_token)->get(
-            uri:'/kanye-rest-quotes',
+            uri:'/kanye-rest-refresh',
         );
 
         $response->assertStatus(200);
-        dd($response->getContent());
+        $this->assertDatabaseHas('quotes', ['quote' => 'example1']);
+        $this->assertDatabaseHas('quotes', ['quote' => 'example2']);
+        $this->assertDatabaseHas('quotes', ['quote' => 'example3']);
+        $this->assertDatabaseHas('quotes', ['quote' => 'example4']);
+        $this->assertDatabaseHas('quotes', ['quote' => 'example5']);
     }
 }
